1019b9a630ff19706f1fb63c37eaff4ffad30f1c
gosunman <gksrmf382@naver.com>
2020-09-17 13:00:09 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+
+function consoleMiddleware(req, res, next) {
+  console.log("I'm a middleware");
+}
+
+function protectedMiddleware(req, res, next) {
+  res.redirect("/");
+}
+
+app.use(consoleMiddleware);
+
+app.get("/about-us", function (req, res) {
+  res.send("About Us.");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Contact");
+});
+
+app.get("/protected", protectedMiddleware, function (req, res) {
+  res.send("Protected");
+});
+
+app.get("/", function (req, res) {
+  res.send("Home");
+});
+
+app.listen(() => console.log(`Listening!`));
